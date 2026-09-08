c7f93a183d498041dea77c1ee56b9b52939d8223
rohedulshah22-rgb <2.93278543e+08+rohedulshah22-rgb@users.noreply.github.com>
2026-09-08 02:34:39 +0000

    fix: align Android Gradle compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         multiDexEnabled = true
-        targetSdk = 35
+        targetSdk = 34
         // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
         // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
         // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
