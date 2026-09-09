c62637d0bb917c14fdd17d3b4aa3b49e476cd630
rohedulshah22-rgb <2.93278543e+08+rohedulshah22-rgb@users.noreply.github.com>
2026-09-09 16:20:56 +0000

    fix: configure Android core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,11 +4,6 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-}
-
 android {
     namespace = "com.novaplay.novaplay"
     compileSdk = 34
@@ -21,25 +16,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.novaplay.novaplay"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         multiDexEnabled = true
         targetSdk = 34
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Debug signing keeps the release artifacts directly installable for testing.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -53,4 +40,9 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
